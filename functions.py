48a7e083b135cafe641a7c650dd932566911ddd9
Ralph McCracken <ralph@mccracken.work>
2023-11-24 12:10:41 -0600

    removed help email validation

--- functions.py
+++ functions.py
@@ -8,8 +8,8 @@ def get_target_email_from_partner_situations(partner, sets):
     df = sets[sets.PARTNER == partner]
     situations = df.SITUATION.values
 
-    for situation in situations:
-        if 'CARILLON' in situation.upper(): return 'carillonbeach@vacayzen.com'
+    # for situation in situations:
+    #     if 'CARILLON' in situation.upper(): return 'carillonbeach@vacayzen.com'
 
     return 'partners@vacayzen.com'
 
